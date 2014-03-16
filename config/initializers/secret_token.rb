8b921b8d7e64a7767d7d58f0a7208844bee0de90
Timmy Martin <timmywmartin@gmail.com>
2014-03-16 15:58:21 -0700

    Created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snapshop::Application.config.secret_key_base = '4e5bb6c596c8773e85f5f289e2ddb8dd7590fb3e5fb0482a1b83824b1bd9e9b81410293769e194339f6ce02df4c85cc84077d2a76b911141c0f568b46218d782'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snapshop::Application.config.session_store :cookie_store, key: '_snapshop_session'
